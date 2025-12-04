81968810fe391be9de792fd0f4b90244eea77c93
Harsheet <hsohaney@mozilla.com>
2025-12-03 20:55:11 -0500

    DAY 2 - part 1 works

---
+++ day_2/productcleaner.py
@@ -0,0 +1,62 @@
+input_file_path = "input.txt"
+
+class Range:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    def __repr__(self):
+        return f"Range({self.min} -- {self.max})"
+
+def validate_pattern(number):
+        strnum = str(number)
+
+        if len(strnum)%2 != 0:
+            return False
+
+        isInvalidProductID = False
+        l = 0
+        r = len(strnum)//2
+
+        while r < len(strnum):
+            if strnum[l] != strnum[r]:
+                return False
+            l+=1
+            r+=1
+        
+        return True
+
+def process_file():
+    ranges = []
+
+    try:
+        with open(input_file_path, 'r') as file:
+            lines = file.readlines()
+
+            for range in lines[0].split(","):
+                curr_min = int(range.split("-")[0])
+                curr_max = int(range.split("-")[1])
+
+                ranges.append(Range(curr_min, curr_max))
+            
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return ranges
+
+def solve():
+    ranges = process_file()
+    count = 0
+
+    for minmax in ranges:
+        for num in range(minmax.min, minmax.max + 1):
+            if validate_pattern(num):
+                count+= num
+                print("FOUND AN ANOMALY", num)
+
+    return count
+
+if __name__ == "__main__":
+    print(solve())
