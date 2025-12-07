6e9d204872a7ae0b2d1f2f3ab8a71556cdc7ccbd
Harsheet <hsohaney@mozilla.com>
2025-12-07 15:47:38 -0500

    Day 5 - Part 2

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,8 +1,44 @@
 use std::fs;
+use itertools::Itertools;
+
+fn merge_ranges(ranges: &Vec<(i64, i64)>) -> i64 {
+    let mut count: i64 = 0;
+    /** 
+     * 3 --- 5
+     *     4 --------  14
+     *       5 --- 10 
+     *               13 --------- 20
+     */
+
+    let (mut lowest_min, mut highest_max) = ranges[0];
+
+    for idx in 1..ranges.len(){
+        let (new_min, new_max) = ranges[idx];
+
+        if new_min > highest_max {
+            count += highest_max - lowest_min + 1;
+
+            lowest_min = new_min;
+            highest_max = new_max;
+        }
+
+        if new_min < lowest_min {
+            lowest_min = new_min;
+        }
+
+        if new_max > highest_max {
+            highest_max = new_max;
+        }
+    }
+
+    count += highest_max - lowest_min + 1;
+
+    count
+}
 
 fn main() {
     let file_contents = fs::read_to_string("input.txt").unwrap();
-    let mut ranges: Vec<Vec<i64>> = Vec::new();
+    let mut ranges: Vec<(i64, i64)> = Vec::new();
     let mut idx: usize = 0;
     for line in file_contents.lines() {
         idx+=1;
@@ -11,8 +47,7 @@ fn main() {
             break;
         }
 
-        let tuple: Vec<i64> = line.split('-').map(|c| c.parse::<i64>().unwrap()).collect();
-        // println!("{:?}",tuple);
+        let tuple: (i64, i64) = line.split('-').map(|c| c.parse::<i64>().unwrap()).collect_tuple().unwrap();
 
         ranges.push(tuple);
     }
@@ -20,26 +55,10 @@ fn main() {
     let mut nums: Vec<i64> = Vec::new();
     for line in file_contents.lines().skip(idx) {
         let num: i64 = line.parse::<i64>().unwrap();
-        // println!("{:?}", num);
         nums.push(num);
     }
 
     ranges.sort();
 
-    let mut count: i32 = 0;
-    for num in nums {
-        for minmax in &ranges {
-            if num < minmax[0] {
-                break;
-            } else if num > minmax[1] {
-                // println!("{num} is spoiled in this");
-                continue;
-            }
-            
-            count += 1;
-            break;
-        }
-    }
-
-    println!("{count}");
+    println!("{:?}", merge_ranges(&ranges));
 }
