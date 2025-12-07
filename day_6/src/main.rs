6f693f035b3b33140112f22ec83e908a4d9ecc96
Harsheet <hsohaney@mozilla.com>
2025-12-07 17:07:40 -0500

    Day 6 - part 1

---
+++ day_6/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+fn parse_input(file_contents: &str) -> Vec<Vec<&str>> {
+    let mut hw = Vec::new();
+
+    for line in file_contents.lines() {
+        let line_vec: Vec<&str> = line.split_whitespace().collect();
+        hw.push(line_vec);
+    }
+
+    hw
+}
+
+fn main() {
+    let file_contents = fs::read_to_string("input.txt").unwrap();
+    let hw = parse_input(&file_contents);
+
+    println!("{:?}", hw);
+
+    let operators = &hw[hw.len() - 1];
+    let numbers_to_count = hw.len();
+    let mut count = 0;
+
+    for idx in 0..hw[0].len() {
+        let mut operation: i64 = 0;
+        match operators[idx] {
+            "*" => {
+                operation = 1;
+                for i in 0..hw.len()-1 {
+                    println!("{:?}", hw[i][idx]);
+                    operation = operation * hw[i][idx].parse::<i64>().unwrap();
+                }
+            },
+            "+" => {
+                for i in 0..hw.len()-1 {
+                    println!("{:?}", hw[i][idx]);
+                    operation = operation + hw[i][idx].parse::<i64>().unwrap();
+                }
+            },
+            &_ => {
+                // do nothing
+            }
+        }
+
+        count += operation;
+    }
+
+    println!("{count}");
+}
